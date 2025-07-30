3a1f19e322e371b5471a6094ecc70d8e607591a6
Fairbear-8974 <112404117+immaculate-lift-studio@users.noreply.github.com>
2025-07-30 10:49:06 -0700

    cleaned up files

--- addons/citycrafter/plugin.gd
+++ addons/citycrafter/plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("CityCrafter", "Node3D", preload("citycrafter.gd"), preload("citycrafter_icon.png"))
+	add_custom_type("CityCrafter", "Node3D", preload("citycrafter.gd"), preload("res://addons/citycrafter/assets/citycrafter_icon.png"))
 
 func _exit_tree():
 	remove_custom_type("CityCrafter")
